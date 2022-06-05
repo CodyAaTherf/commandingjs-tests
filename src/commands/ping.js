6959c2f8ac832ff4d9cb5a27950a33ffa96feffc
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-06-05 13:14:32 +0530

    permissions

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ping' ,
     commands: ['p'] , // or aliases: ['p'] . either works
     description: 'Ping' ,
+    requiredPermissions: ['ADMINISTRATOR'] ,
     callback: (message) => {
       message.reply('Pong!')
     }
